0c7173eab35dc357a8263211f5a2ed22720eacad
enRails <info@enrails.com>
2012-04-03 19:24:55 +0200

    genericas tienen etiquetas etiquetas tienen genericas

--- app/models/generica.rb
+++ app/models/generica.rb
@@ -1,4 +1,8 @@
 class Generica < ActiveRecord::Base
     mount_uploader :imagen,ImagenUploader
     has_and_belongs_to_many :labels
+    
+    def etiquetas
+       self.labels.collect { |l| l.nombre+' ' } 
+    end
 end
