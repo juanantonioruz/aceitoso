2e79fd0a2c77e7a6615b79e81137a9fad697bb5f
enRails <info@enrails.com>
2012-04-03 16:59:06 +0200

    ordenando labels jquery

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,5 +1,9 @@
+
+
 class Label < ActiveRecord::Base
   belongs_to :label
   has_many :labels,:dependent=>:destroy
-   has_and_belongs_to_many :genericas
+  has_and_belongs_to_many :genericas
+  
+ 
 end
