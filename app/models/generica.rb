6bf7c8e2685ae8748d368f753cf79343af2b915e
enRails <info@enrails.com>
2012-04-03 21:03:14 +0200

    imagenes imagen(modelo) imagentam(modelo,tam)

--- app/models/generica.rb
+++ app/models/generica.rb
@@ -1,8 +1,11 @@
 class Generica < ActiveRecord::Base
+    
     mount_uploader :imagen,ImagenUploader
     has_and_belongs_to_many :labels
     
     def etiquetas
        self.labels.collect { |l| l.nombre+' ' } 
     end
+   
+    
 end
