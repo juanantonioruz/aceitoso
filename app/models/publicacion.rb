0dcb0b131d6b7a852f7ba04b702fa6c36a1f7925
enRails <info@enrails.com>
2012-04-11 15:47:42 +0200

    add http to url

--- app/models/publicacion.rb
+++ app/models/publicacion.rb
@@ -2,21 +2,21 @@ class Publicacion < ActiveRecord::Base
   belongs_to :museo
   has_and_belongs_to_many :labels,:join_table=>'publicacions_labels'
 
-  before_validation :smart_add_url_protocol
+  before_save :smart_add_url_protocol
 
 
 
     def smart_add_url_protocol
-        unless (self.url[/^http?s:\/\//] || self.url=='')
-         self.url = 'http://' + self.url
-         end
+        unless self.url.include?("http://")
+          self.url = 'http://' + self.url
+        end
     end
 
     def enlace
         if self.url==''
             return ' '
         else
-            return '<a href='+self.url+'>'+self.url+'</a>'
+            return self.url
         end
     end
 end
