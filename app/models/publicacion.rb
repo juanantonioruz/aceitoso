8f6c3966aad978c5ca301fbf1b61d83d58a341a8
enRails <info@enrails.com>
2012-04-11 15:34:21 +0200

    publicaciones index tags

--- app/models/publicacion.rb
+++ app/models/publicacion.rb
@@ -1,4 +1,22 @@
 class Publicacion < ActiveRecord::Base
   belongs_to :museo
-    has_and_belongs_to_many :labels,:join_table=>'publicacions_labels'
+  has_and_belongs_to_many :labels,:join_table=>'publicacions_labels'
+
+  before_validation :smart_add_url_protocol
+
+
+
+    def smart_add_url_protocol
+        unless (self.url[/^http?s:\/\//] || self.url=='')
+         self.url = 'http://' + self.url
+         end
+    end
+
+    def enlace
+        if self.url==''
+            return ' '
+        else
+            return '<a href='+self.url+'>'+self.url+'</a>'
+        end
+    end
 end
