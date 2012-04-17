bbed3fe1bfcedd0310f11b1f4f371847a5d57f58
Miguel Diaz <migueldiazgajete@gmail.com>
2012-04-17 12:24:03 +0200

    copy

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -53,13 +52,6 @@ ActiveRecord::Schema.define(:version => 20120417005636) do
   end
 
   add_index "coordenadas", ["camino_id"], :name => "index_coordenadas_on_camino_id"
-
-  create_table "elemento_relacionables", :force => true do |t|
-    t.integer  "heir_id"
-    t.string   "heir_type"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
   create_table "entornos", :force => true do |t|
     t.integer "museo_id"
