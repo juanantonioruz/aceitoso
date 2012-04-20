3a8c14badd006bde0799d9442605fbc28124cb62
juanantonioruz <juanantonioruz@gmail.com>
2012-04-20 02:12:48 +0200

    por fin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,7 @@ Olearum::Application.configure do
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
+   config.log_level = :warn
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
