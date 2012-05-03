942725e1a5db4673a9ad6d61bec43ed8c817b3ac
juanantonioruz <juanantonioruz@gmail.com>
2012-05-04 01:44:24 +0200

    bug de pintado de relaciones y sentido

--- app/models/sentido_relacion.rb
+++ app/models/sentido_relacion.rb
@@ -25,6 +25,18 @@ class SentidoRelacion < ActiveRecord::Base
     end
     
     end
+
+    def titulo_desde_destino
+      logger.info(creciente)
+      if(creciente.to_s=="0")
+      logger.info("+++++++++++++++++++++++++++++++++++++++++++++++ self.creciente")
+        return "> #{nombre_relacion.nombre2} > "
+      else
+      logger.info("-------------------------------------- self.decreciente")
+        return "> #{nombre_relacion.nombre1} > "
+    end
+    end
+    
     def titulo_destino_desde_destino
         titulo_destino.gsub(">", "")
     
