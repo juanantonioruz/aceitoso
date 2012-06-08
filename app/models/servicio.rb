56a4668a451210fcec240520066d13086f92f429
enRails <info@enrails.com>
2012-06-08 13:47:34 +0200

    servicio.imagen devuelve la imagen del service para simplificar la consulta

--- app/models/servicio.rb
+++ app/models/servicio.rb
@@ -1,4 +1,12 @@
 class Servicio < ActiveRecord::Base
   belongs_to :museo
   belongs_to :service
+
+  def imagen
+    if self.service!=nil
+        return self.service.imagen
+    else
+        return ""
+    end
+  end
 end
