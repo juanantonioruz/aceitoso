9015210447b03372a4aa2ef0bfd95912fb1f0e13
juanantonioruz <juanantonioruz@gmail.com>
2012-06-28 01:11:32 +0200

    logg

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -38,6 +38,7 @@ end
       if(!id.nil?) then
         if (id[0]=="g") then 
         Relacionable.find(id[1..id.length]).heir.piezas.each{|pieza| id_arra << pieza.museo.predecessor.id.to_s}
+        puts "“#{id_arra} ----------------------------"
       elsif (id[0]=="i") then
         
         Relacionable.find(id[1..id.length]).heir.generica.piezas.each{|pieza| id_arra << pieza.museo.predecessor.id.to_s}
