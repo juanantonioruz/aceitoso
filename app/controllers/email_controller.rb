4f8cacb25388985b5dc7a541047665b0c11f5f85
juanantonioruz <juanantonioruz@gmail.com>
2012-07-03 01:41:40 +0200

    ya pille el error Closes #211

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -7,11 +7,11 @@ class EmailController < ApplicationController
   	def create
     	@email = Email.new(params[:email])
     	if @email.valid?
-      		ContactoMailer.contactar(@email, "juanantonioruz@gmail.com, arquibernardo@gmail.com, marionn.pa@gmail.com, info@enjava.com").deliver
-          @mensaje="buen envio"
+      		ContactoMailer.contactar(@email, "juanantonioruz@gmail.com, arquibernardo@gmail.com, marionn.pa@gmail.com").deliver
+          @mensaje="el formulario de contacto se ha enviado correctamente"
       		#redirect_to(@email.url, :notice => t("enviook"))
     	else
-          @mensaje="mal envio"
+          @mensaje="no se ha podido enviar el formulario, pruebe otra vez rellenando todos los campos del formulario"
       	#redirect_to(@url,:notice=>t('enviobad'))
       	
     	end
